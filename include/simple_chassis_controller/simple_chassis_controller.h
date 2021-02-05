f70df6df8790a351ee8d9bc3ce273a751e57c0fb
qiayuan <liaoqiayuan@gmail.com>
2021-02-06 03:23:18 +0800

    Add some basic code

--- include/simple_chassis_controller/simple_chassis_controller.h
+++ include/simple_chassis_controller/simple_chassis_controller.h
@@ -5,4 +5,21 @@
 #ifndef SIMPLE_CHASSIS_CONTROLLER_SIMPLE_CHASSIS_CONTROLLER_H
 #define SIMPLE_CHASSIS_CONTROLLER_SIMPLE_CHASSIS_CONTROLLER_H
 
+#include <controller_interface/controller.h>
+#include <hardware_interface/joint_command_interface.h>
+
+namespace simple_chassis_controller {
+class SimpleChassisController : public controller_interface::Controller<hardware_interface::EffortJointInterface> {
+ public:
+  SimpleChassisController() = default;
+  ~SimpleChassisController() override = default;
+
+  bool init(hardware_interface::EffortJointInterface *robot,
+            ros::NodeHandle &root_nh, ros::NodeHandle &controller_nh) override;
+
+  void update(const ros::Time &time, const ros::Duration &period) override;
+
+};
+}
+
 #endif //SIMPLE_CHASSIS_CONTROLLER_SIMPLE_CHASSIS_CONTROLLER_H
--- src/simple_chassis_controller.cpp
+++ src/simple_chassis_controller.cpp
@@ -3,3 +3,18 @@
 //
 
 #include "simple_chassis_controller/simple_chassis_controller.h"
+#include <pluginlib/class_list_macros.hpp>
+
+namespace simple_chassis_controller {
+bool SimpleChassisController::init(hardware_interface::EffortJointInterface *robot,
+                                   ros::NodeHandle &root_nh, ros::NodeHandle &controller_nh) {
+
+  return true;
+}
+
+void SimpleChassisController::update(const ros::Time &time, const ros::Duration &period) {
+
+}
+
+PLUGINLIB_EXPORT_CLASS(simple_chassis_controller::SimpleChassisController, controller_interface::ControllerBase)
+}
