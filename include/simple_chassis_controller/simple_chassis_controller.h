89767c5bf2bd0677d3cc85b2f7df30cb56e7f942
qiayuan <liaoqiayuan@gmail.com>
2021-02-06 04:52:47 +0800

    Finish and test simple_chassis_controller

--- include/simple_chassis_controller/simple_chassis_controller.h
+++ include/simple_chassis_controller/simple_chassis_controller.h
@@ -9,17 +9,22 @@
 #include <hardware_interface/joint_command_interface.h>
 
 namespace simple_chassis_controller {
+
 class SimpleChassisController : public controller_interface::Controller<hardware_interface::EffortJointInterface> {
  public:
   SimpleChassisController() = default;
   ~SimpleChassisController() override = default;
 
-  bool init(hardware_interface::EffortJointInterface *robot,
+  bool init(hardware_interface::EffortJointInterface *effort_joint_interface,
             ros::NodeHandle &root_nh, ros::NodeHandle &controller_nh) override;
 
   void update(const ros::Time &time, const ros::Duration &period) override;
 
+  hardware_interface::JointHandle front_left_joint_, front_right_joint_, back_left_joint_, back_right_joint_;
+ private:
+  int state_;
+  ros::Time last_change_;
 };
-}
+}// namespace simple_chassis_controller
 
 #endif //SIMPLE_CHASSIS_CONTROLLER_SIMPLE_CHASSIS_CONTROLLER_H
--- src/simple_chassis_controller.cpp
+++ src/simple_chassis_controller.cpp
@@ -6,14 +6,35 @@
 #include <pluginlib/class_list_macros.hpp>
 
 namespace simple_chassis_controller {
-bool SimpleChassisController::init(hardware_interface::EffortJointInterface *robot,
+bool SimpleChassisController::init(hardware_interface::EffortJointInterface *effort_joint_interface,
                                    ros::NodeHandle &root_nh, ros::NodeHandle &controller_nh) {
+  front_left_joint_ = effort_joint_interface->getHandle("front_left_wheel_joint");
+  front_right_joint_ = effort_joint_interface->getHandle("front_right_wheel_joint");
+  back_left_joint_ = effort_joint_interface->getHandle("back_left_wheel_joint");
+  back_right_joint_ = effort_joint_interface->getHandle("back_right_wheel_joint");
 
   return true;
 }
 
 void SimpleChassisController::update(const ros::Time &time, const ros::Duration &period) {
-
+  double tau = 0.2;  // torque
+  // NOTE: DON'T COPY THESE NAIVE TESTING CODES !!! USE INVERSE KINEMATICS !!!
+  // NOTE: DON'T COPY THESE NAIVE TESTING CODES !!! USE INVERSE KINEMATICS !!!
+  // NOTE: DON'T COPY THESE NAIVE TESTING CODES !!! USE INVERSE KINEMATICS !!!
+  static double cmd_[6][4] = {{tau, tau, tau, tau}, //  forward
+                              {-2 * tau, -2 * tau, -2 * tau, -2 * tau}, //  backward
+                              {-tau, tau, tau, -tau}, //  left
+                              {2 * tau, -2 * tau, -2 * tau, 2 * tau}, //  right
+                              {2 * tau, -2 * tau, 2 * tau, -2 * tau}, //  clockwise
+                              {-tau, tau, -tau, tau}};  //  counterclockwise
+  if ((time - last_change_).toSec() > 2) {
+    state_ = (state_ + 1) % 6;
+    last_change_ = time;
+  }
+  front_left_joint_.setCommand(cmd_[state_][0]);
+  front_right_joint_.setCommand(cmd_[state_][1]);
+  back_left_joint_.setCommand(cmd_[state_][2]);
+  back_right_joint_.setCommand(cmd_[state_][3]);
 }
 
 PLUGINLIB_EXPORT_CLASS(simple_chassis_controller::SimpleChassisController, controller_interface::ControllerBase)
